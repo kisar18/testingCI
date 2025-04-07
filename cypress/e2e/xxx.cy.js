95d027d2ca11198d0be047d328273a1c426ad106
kisar18 <martinpur99@seznam.cz>
2025-04-07 17:02:30 +0200

    test CI3

--- cypress/e2e/xxx.cy.js
+++ cypress/e2e/xxx.cy.js
@@ -2,7 +2,6 @@ describe('Verify Browser Stack Home Page', () => {
 
   it('Verify Browserstack logo is visible', () => {
       cy.visit('https://www.browserstack.com/');
-      cy.get('#logo').should('be.visible');
   })
 
   it('Click on Sign In', () => {
