58075774c589e2a7019bca1c5fec7892f7d3557a
kisar18 <martinpur99@seznam.cz>
2025-04-07 16:59:22 +0200

    test CI2

--- cypress/e2e/xxx.cy.js
+++ cypress/e2e/xxx.cy.js
@@ -5,10 +5,9 @@ describe('Verify Browser Stack Home Page', () => {
       cy.get('#logo').should('be.visible');
   })
 
-  it.only('Click on Sign In', () => {
+  it('Click on Sign In', () => {
     cy.visit('https://www.browserstack.com/');
     cy.get('a[title="Sign in"]').then(($selectedElement) => {
-      debugger;
       $selectedElement.get(0).click()
     })
   })
